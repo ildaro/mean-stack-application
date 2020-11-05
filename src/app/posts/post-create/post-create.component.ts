5d383384f52182df66a17cc18b80ecad9921a30e
ildar khatipov <ildar.k@hotmail.co.uk>
2020-11-05 12:06:58 +0000

    installed angular material and added styling to post-create component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,14 +3,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-post-create',
-    templateUrl: './post-create.component.html'
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
 })
 
 export class PostCreateComponent {
   newPost = 'NO CONTENT';
   enteredValue = '';
 
-  onAddPost(postInput: HTMLTextAreaElement) {
+  onAddPost() {
     this.newPost = this.enteredValue;
   }
 }
