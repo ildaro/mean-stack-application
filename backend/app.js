3fba6d9046bb0489c10c43fee21ce6ca7ba13fb5
ildar khatipov <ildar.k@hotmail.co.uk>
2020-12-08 16:58:48 +0000

    added POST api endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const { stringify } = require('querystring');
+const bodyParser = require('body-parser');
 
 const app = express(); //execute express as a function and store in app
 
- //setting headers to solve CORS error
+app.use(bodyParser.json()); //express middleware for parsing json data
+
+//setting headers to solve CORS error
 app.use((req, res, next)=> {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"); //which http verbs are allowed to be sent
   next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "post added successfully"
+  }); //status code for new resource was created and everything is ok
 });
 
 app.use("/api/posts",(req, res, next) => { //requests going to api/posts will reach this code
