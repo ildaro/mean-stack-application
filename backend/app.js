3ebf569a08fdabf7afbb2289c5d7b3a9b50b6c6b
ildar khatipov <ildar.k@hotmail.co.uk>
2020-12-08 16:35:25 +0000

    set headers to fix CORS error

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,14 @@ const { stringify } = require('querystring');
 
 const app = express(); //execute express as a function and store in app
 
+ //setting headers to solve CORS error
+app.use((req, res, next)=> {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"); //which http verbs are allowed to be sent
+  next();
+});
+
 app.use("/api/posts",(req, res, next) => { //requests going to api/posts will reach this code
   const posts = [
     {id: "0dummyid", title: "dummy server side post 1", content: "some content from the server"},
