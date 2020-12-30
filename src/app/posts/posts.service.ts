ac628347f41a67a5e3446118b233eec01544251d
ildar khatipov <ildar.k@hotmail.co.uk>
2020-12-30 14:31:57 +0000

    transformed response data in backend using map

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model'
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'}) //this makes it usable throughout the app and there can only be one instance
 export class PostsService{
@@ -12,9 +13,18 @@ export class PostsService{
 
   //retrieve posts
   getPosts(){
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return{
+            title: post.title,
+            content: post.content,
+            id: post._id
+          }; //map function to make 'id' to be '_id'
+        });
+      }))
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       }); //subscribe to http observable, no need to unsubscribe because httpclient is a built in angular thing which will handle it for me
   }
