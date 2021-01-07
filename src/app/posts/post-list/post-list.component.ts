416f35d128e5c723396cdb9145955e639d868746
ildar khatipov <ildar.k@hotmail.co.uk>
2021-01-07 13:42:51 +0000

    connected angular pagination to the backend

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -17,6 +17,7 @@ export class PostListComponent implements OnInit, OnDestroy{
   isLoading = false; //for the spinner
   totalPosts = 10; //for paginator
   postsPerPage = 2;
+  currentPage = 1;
   pageSizeOptions = [1,2,5,10];
   private postsSub: Subscription;
 
@@ -24,7 +25,7 @@ export class PostListComponent implements OnInit, OnDestroy{
 
   ngOnInit(){
     this.isLoading = true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((posts: Post[]) => {
         this.isLoading = false;
@@ -38,7 +39,9 @@ export class PostListComponent implements OnInit, OnDestroy{
 
   //to handle pages being changed
   onChangedPage(pageData: PageEvent){
-    console.log(pageData);
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
 
   ngOnDestroy(){
