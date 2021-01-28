a2b70850bb21962444dad2a8830e90fff8ba27f4
ildar khatipov <ildar.k@hotmail.co.uk>
2021-01-28 18:06:33 +0000

    installed JSON webtoken for authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bcrypt = require("bcrypt"); // to encrypt user passwords
+const jwt = require('jsonwebtoken');
 
 const User = require("../models/user");
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
+  bcrypt.hash(req.body.password, 10) //hash the password for security
     .then(hash => {
       const user = new User({
         email: req.body.email,
@@ -24,7 +25,35 @@ router.post("/signup", (req, res, next) => {
           });
         });
     });
+});
 
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(!user){ //if the user doesnt exist then no
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password); //returns true if the password matches one in the DB
+    })
+    .then(result => {
+      if (!result){
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      const token = jwt.sign( //create a jsonwebtoken
+        { email: user.email, userId: user._id },
+         'secret_this_should_be_longer',
+        { expiresIn: "1h"} // set token to expire in 1hr
+        );
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+    })
+  });
 });
 
 module.exports = router;
