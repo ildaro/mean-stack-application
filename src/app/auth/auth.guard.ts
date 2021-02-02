cb4b05de8ee71a7e0f91a0364fbce8a26345a25b
ildar khatipov <ildar.k@hotmail.co.uk>
2021-02-02 12:41:45 +0000

    added route guards for /create and /edit

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authSerivce: AuthService, private router: Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    const isAuth = this.authSerivce.getIsAuth();
+    if (!isAuth){
+      this.router.navigate(['/login']);
+    }
+    return isAuth;
+  }
+}
