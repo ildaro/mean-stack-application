0a8cac041089575f126fdfaf228779cdead84bd6
ildar khatipov <ildar.k@hotmail.co.uk>
2021-02-02 12:03:22 +0000

    users are now redirected to the home page after logging in/out

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Subject } from "rxjs";
 
 import { AuthData } from "./auth-data.model";
@@ -10,7 +11,7 @@ export class AuthService {
   private token: string;
   private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   createUser(email: string, password: string){
     const authData: AuthData = {email: email, password: password};
@@ -29,6 +30,7 @@ export class AuthService {
         if (token) {
           this.isAuthenticated = true;
           this.authStatusListener.next(true); //to know if the user is autherized using authStatusListener
+          this.router.navigate(['/']); //redirect to homepage after logging in
         }
       });
   }
@@ -50,5 +52,6 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    this.router.navigate(['/']); //redirect to homepage after logging out
   }
 }
