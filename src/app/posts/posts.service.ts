a307ec07ec14e2410dedb1ded60234a5520f3770
ildar khatipov <ildar.k@hotmail.co.uk>
2021-02-03 11:27:44 +0000

    added User ID to posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -22,7 +22,8 @@ export class PostsService{
             title: post.title,
             content: post.content,
             id: post._id,
-            imagePath: post.imagePath
+            imagePath: post.imagePath,
+            creator: post.creator
           }; //map function to make 'id' to be '_id'
         }), maxPosts: postData.maxPosts};
       }))
