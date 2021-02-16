cbe48600d35bba518dfc63e2c2123db53bd0af03
ildar khatipov <ildar.k@hotmail.co.uk>
2021-02-16 18:58:34 +0000

    added MatDialog to display basic error dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -20,6 +21,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './error/error.component';
 
 
 
@@ -31,6 +33,7 @@ import { ErrorInterceptor } from './error-interceptor';
     PostListComponent,
     LoginComponent,
     SignupComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
@@ -41,6 +44,7 @@ import { ErrorInterceptor } from './error-interceptor';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule,
     MatToolbarModule,
     MatExpansionModule,
     MatProgressSpinnerModule,
--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -1,16 +1,18 @@
 import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { ErrorComponent } from "./error/error.component";
 
-
-//using HttpInterceptor to add token to request going to the backend
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
+  constructor(private dialog: MatDialog){}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(error);
-        alert(error.error.message);
+        this.dialog.open(ErrorComponent);
         return throwError(error)
       })
     );
