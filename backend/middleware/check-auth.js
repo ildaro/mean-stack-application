bce50aff67a3d0c80b2f3ae56be3b9e490e182af
ildar khatipov <ildar.k@hotmail.co.uk>
2021-02-21 04:04:10 +0000

    using node environment variable for JWT key

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1]; //get token from incoming req in the header
-    const decodedToken = jwt.verify(token, "secret_this_should_be_longer"); // verify the token using the signature from when token was created
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY); // verify the token using the signature from when token was created
     req.userData = { email: decodedToken.email, userId: decodedToken.userId };
     next();
   } catch(error) {
